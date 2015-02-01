08d40b3791988129cfd27a52dba8dffa000205db
Stefan Niederhauser <ghuder5@gmx.ch>
2015-02-01 15:51:43 +0100

    problem 27

---
+++ project-euler/problem27.py
@@ -0,0 +1,16 @@
+from sieve import sieve, is_prime
+
+s = sieve(100000)
+
+m = 0
+c = 0
+for a in xrange(-999, 1000):
+    for b in xrange(-999, 1000):
+        n = 0
+        while is_prime(s, n * n + a * n + b): n += 1
+        if n > m:
+            print n, a, b
+            m = n
+            c = a * b
+
+print c
